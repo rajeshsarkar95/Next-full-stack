769c18de6e02600ad3493008846db4f43624cf8b
Rajesh Sarkar <Rajeshsarkar@gmail.com>
2025-06-04 20:39:38 +0530

    [id]

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,19 +1,17 @@
+// File: src/app/profile/[id]/page.tsx
+
 import React from "react";
 
-type UserProfileProps = {
+interface PageProps {
   params: {
     id: string;
   };
-};
-export default function userProfile({ params }:UserProfileProps) {
+}
+export default function UserProfilePage({ params }: PageProps) {
   return (
-    <div>
-      <h1>Profile</h1>
-      <hr />
-       <p>
-        profile page
-        <span>{params.id}</span>
-      </p>
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-4">Profile Page</h1>
+      <p className="text-white">User ID: {params.id}</p>
     </div>
   );
 }
